6fff26d1517dbcec6dfb3f181bda418577ea3b17
Jacek Kowalski <Jacek@jacekk.info>
2023-11-24 13:16:31 +0100

    ContextTypeHelper: do account for the "Accept" header (per CAS specs)

--- src/test/java/org/keycloak/protocol/cas/ContentTypeHelperTest.java
+++ src/test/java/org/keycloak/protocol/cas/ContentTypeHelperTest.java
@@ -13,33 +13,15 @@ import static org.junit.Assert.assertEquals;
 public class ContentTypeHelperTest {
     @Test
     public void test() throws Exception {
-        assertEquals(MediaType.APPLICATION_XML_TYPE, get("http://example.com/", null).selectResponseType());
-        assertEquals(MediaType.APPLICATION_JSON_TYPE, get("http://example.com/?format=json", null).selectResponseType());
-        assertEquals(MediaType.APPLICATION_XML_TYPE, get("http://example.com/?format=xml", null).selectResponseType());
-        assertEquals(MediaType.APPLICATION_JSON_TYPE, get("http://example.com/?format=JSON", null).selectResponseType());
-        assertEquals(MediaType.APPLICATION_XML_TYPE, get("http://example.com/?format=XML", null).selectResponseType());
-
-        assertEquals(MediaType.APPLICATION_XML_TYPE, get("http://example.com/", MediaType.APPLICATION_XML).selectResponseType());
-        assertEquals(MediaType.APPLICATION_JSON_TYPE, get("http://example.com/?format=json", MediaType.APPLICATION_XML).selectResponseType());
-        assertEquals(MediaType.APPLICATION_XML_TYPE, get("http://example.com/?format=xml", MediaType.APPLICATION_XML).selectResponseType());
-
-        assertEquals(MediaType.APPLICATION_JSON_TYPE, get("http://example.com/", MediaType.APPLICATION_JSON).selectResponseType());
-        assertEquals(MediaType.APPLICATION_JSON_TYPE, get("http://example.com/?format=json", MediaType.APPLICATION_JSON).selectResponseType());
-        assertEquals(MediaType.APPLICATION_XML_TYPE, get("http://example.com/?format=xml", MediaType.APPLICATION_JSON).selectResponseType());
-
-        assertEquals(MediaType.APPLICATION_XML_TYPE, get("http://example.com/", MediaType.TEXT_PLAIN).selectResponseType());
-        assertEquals(MediaType.APPLICATION_XML_TYPE, get("http://example.com/", "text/html, image/gif, image/jpeg, *; q=.2, */*; q=.2").selectResponseType());
+        assertEquals(MediaType.APPLICATION_XML_TYPE, get("http://example.com/").selectResponseType());
+        assertEquals(MediaType.APPLICATION_JSON_TYPE, get("http://example.com/?format=json").selectResponseType());
+        assertEquals(MediaType.APPLICATION_XML_TYPE, get("http://example.com/?format=xml").selectResponseType());
+        assertEquals(MediaType.APPLICATION_JSON_TYPE, get("http://example.com/?format=JSON").selectResponseType());
+        assertEquals(MediaType.APPLICATION_XML_TYPE, get("http://example.com/?format=XML").selectResponseType());
     }
 
-    private ContentTypeHelper get(String uri, String acceptHeader) throws Exception {
+    private ContentTypeHelper get(String uri) throws Exception {
         MockHttpRequest req = MockHttpRequest.get(uri);
-        MockHttpResponse res = new MockHttpResponse();
-        RequestImpl restReq = new RequestImpl(req, res);
-
-        if (acceptHeader != null) {
-            req = req.header(HttpHeaders.ACCEPT, acceptHeader);
-        }
-
-        return new ContentTypeHelper(req, restReq, req.getUri());
+        return new ContentTypeHelper(req.getUri());
     }
 }
