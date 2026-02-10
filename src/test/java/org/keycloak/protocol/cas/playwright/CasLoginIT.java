528dfe6b800612ab5d410e985d6e71a26f286851
Robin Meese <39960884+robson90@users.noreply.github.com>
2026-02-10 10:41:14 +0100

    handle PR comments
    Signed-off-by: Robin Meese <39960884+robson90@users.noreply.github.com>

--- src/test/java/org/keycloak/protocol/cas/playwright/CasLoginIT.java
+++ src/test/java/org/keycloak/protocol/cas/playwright/CasLoginIT.java
@@ -1,7 +1,10 @@
-package org.keycloak.protocol.cas;
+package org.keycloak.protocol.cas.playwright;
 
 import com.microsoft.playwright.Page;
 import dasniko.testcontainers.keycloak.KeycloakContainer;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,7 +12,6 @@ import org.junit.jupiter.api.Test;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.resource.RealmResource;
 import org.keycloak.admin.client.resource.UsersResource;
-import org.keycloak.protocol.cas.playwright.PlaywrightDefaults;
 import org.keycloak.representations.idm.CredentialRepresentation;
 import org.keycloak.representations.idm.RealmRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
@@ -24,9 +26,12 @@ import java.util.concurrent.atomic.AtomicReference;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.keycloak.common.util.Encode.urlEncode;
+import static org.keycloak.protocol.cas.playwright.PlaywrightHelper.assertOnGivenUserData;
+import static org.keycloak.protocol.cas.playwright.PlaywrightHelper.fillKeycloakLoginForm;
+import static org.keycloak.protocol.cas.playwright.PlaywrightHelper.goToRealmAndCreateNewClient;
 
 @Testcontainers
-public class TicketIT {
+public class CasLoginIT {
   private static final String ADMIN_USER = "admin";
   private static final String ADMIN_PASSWORD = "keycloak";
   private static final String REALM_NAME = "cas-realm";
@@ -90,9 +95,14 @@ public class TicketIT {
     playwrightDefaults.tearDown();
   }
 
+  @AfterAll
+  static void afterAll() {
+    keycloakClient.realms().realm(REALM_NAME).remove();
+  }
+
   @Test
   void assertTicketContent() {
-    goToRealmAndCreateNewClient();
+    goToRealmAndCreateNewClient(page, keycloakClient, keycloakContainer.getAuthServerUrl(), REALM_NAME, CAS_CLIENT_1, ADMIN_USER, ADMIN_PASSWORD, SERVICE_URL_1);
 
     // Trigger CAS authorization and login fully in the browser
     String casLoginUrl = keycloakContainer.getAuthServerUrl() + "/realms/" + REALM_NAME + "/protocol/cas/login?service=" + urlEncode(SERVICE_URL_1);
@@ -108,7 +118,7 @@ public class TicketIT {
         ticketUrl.set(url);
       }
     });
-    fillKeycloakLoginForm(TEST_USERNAME, TEST_USER_PASSWORD);
+    fillKeycloakLoginForm(page, TEST_USERNAME, TEST_USER_PASSWORD);
 
     // Redirect after successful auth
     assertTrue(ticketUrl.get() != null && ticketUrl.get().contains("ticket="));
@@ -116,13 +126,20 @@ public class TicketIT {
     String ticket = getQueryParam(finalUrl, "ticket");
     assertNotNull(ticket, "Ticket not present in final URL");
 
-    // Call validate URL and assert UserData against TicketData.
-    String validateUrl = keycloakContainer.getAuthServerUrl() + "/realms/" + REALM_NAME + "/protocol/cas/serviceValidate?service=" + urlEncode(SERVICE_URL_1) + "&ticket=" + urlEncode(ticket);
-    page.close();
-    page = playwrightDefaults.setUp();
-    page.navigate(validateUrl);
-    page.waitForSelector("text=<cas:authenticationSuccess>");
-    assertOnGivenUserData(page.content(), TEST_USERNAME, TEST_USER_FIRST_NAME, TEST_USER_LAST_NAME);
+    Response validate =
+        RestAssured.given()
+            .queryParam("service", SERVICE_URL_1)
+            .queryParam("ticket", ticket)
+            .get(String.format("%s/realms/%s/protocol/cas/serviceValidate", keycloakContainer.getAuthServerUrl(), REALM_NAME))
+            .then()
+            .statusCode(200)
+            .extract()
+            .response();
+
+    String xmlData = validate.asString();
+
+    assertTrue(xmlData.contains("<cas:authenticationSuccess>"), "Expected authenticationSuccess in CAS XML");
+    assertOnGivenUserData(xmlData, TEST_USERNAME, TEST_USER_FIRST_NAME, TEST_USER_LAST_NAME);
   }
 
   private static String getQueryParam(String url, String name) {
@@ -140,58 +157,6 @@ public class TicketIT {
     } catch (Exception ignored) {
     }
     return null;
-  }
-
-  private static void assertOnGivenUserData(String xmlCasData, String username, String firstName, String lastName) {
-    assertTrue(xmlCasData.contains(String.format("<cas:user>%s</cas:user>", username)));
-    assertTrue(xmlCasData.contains(String.format("<cas:mail>%s</cas:mail>", username)));
-    assertTrue(xmlCasData.contains(String.format("<cas:givenName>%s</cas:givenName>", firstName)));
-    assertTrue(xmlCasData.contains(String.format("<cas:sn>%s</cas:sn>", lastName)));
-    assertTrue(xmlCasData.contains(String.format("<cas:cn>%s %s</cas:cn>", firstName, lastName)));
-  }
-
-  private void fillKeycloakLoginForm(String username, String password) {
-    page.waitForSelector("#username").isVisible();
-    page.fill("#username", username);
-    page.fill("#password", password);
-    page.click("#kc-login");
-  }
-
-  private void goToRealmAndCreateNewClient() {
-    // Login into Keycloak Admin Console
-    String baseUrl = keycloakContainer.getAuthServerUrl();
-    page.navigate(baseUrl + "/admin/");
-    fillKeycloakLoginForm(ADMIN_USER, ADMIN_PASSWORD);
-    page.waitForSelector("#nav-item-realms");
-    // Keycloak UI has been loaded
-
-    // Switching Realm
-    page.click("#nav-item-realms");
-    page.click(String.format("text=%s", REALM_NAME));
-    // Create Client
-    page.click("#nav-item-clients");
-    page.click(String.format("text=%s", "Create client"));
-    page.click("#protocol");
-    // Wait for Dropdown to be visible
-    String casButton = "button[role='option']:has-text('cas')";
-    page.waitForSelector(casButton);
-    page.locator(casButton).click();
-    page.fill("#clientId", CAS_CLIENT_1);
-    page.fill("#name", CAS_CLIENT_1.toUpperCase());
-    page.click(String.format("text=%s", "Next"));
-    // Capability Page
-    page.click(String.format("text=%s", "Next"));
-    page.getByTestId("redirectUris0").fill(SERVICE_URL_1);
-    page.getByLabel("Valid post logout redirect URIs").fill(SERVICE_URL_1);
-    page.getByLabel("Web origins").fill("*");
-    page.click(String.format("text=%s", "Save"));
-    // Client Creation done
-    assertTrue(page.waitForSelector("#general-settings").isVisible());
-
-    // verify the correct parameters were inserted into the UI
-    var casClient = keycloakClient.realm(REALM_NAME).clients().findByClientId(CAS_CLIENT_1).get(0);
-    assertTrue(casClient.getRedirectUris().contains(SERVICE_URL_1));
-    assertTrue(casClient.getWebOrigins().contains("*"));
   }
 }
 
